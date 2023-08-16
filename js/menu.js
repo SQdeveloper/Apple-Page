9e207124ca9e85480217e0063ad23a7803198133
Jefferson <jefferson.silvaquinto@gmail.com>
2023-08-15 22:23:29 -0500

    adding menu responsive

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+const menu         = document.querySelector(".menu");
+const button_arrow = document.querySelector(".ul_responsive button");
+const background   = document.querySelector(".background_blur");
+let isActive       = true;
+
+button_arrow.addEventListener("click", ()=> {
+    menu.classList.toggle("active");
+    background.classList.toggle("active");
+    if(isActive) {
+        button_arrow.querySelector("img").src = "../assets/icons/chevron-up.svg";
+    } else {
+        button_arrow.querySelector("img").src = "../assets/icons/chevron-down.svg";
+    }
+    isActive = !isActive;
+});
+
+background.addEventListener("click", ()=>{
+    background.classList.remove("active");
+    menu.classList.remove("active");
+});
+
+menu.addEventListener("scroll", ()=>{
+    console.log("scroll")
+});
